ca8b07c58c054cff2d5ad55e9c50630c4ef71cf8
smyrgeorge <smyrgeorge@smyrgeorge-K55VJ>
2014-04-01 03:35:04 +0300

    Merge origin/master

--- src/informationretrieval/Main.java
+++ src/informationretrieval/Main.java
@@ -16,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("sdsd");
+        System.out.println("sdnbmnbmsd");
         // TODO code application logic here
     }
     
