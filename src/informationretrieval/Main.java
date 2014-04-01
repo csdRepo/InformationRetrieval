5e652a010b5987f42d961d09a531acb43baec6a0
Jmoschon <Jmoschon@Mosxonas>
2014-04-01 13:17:00 +0300

    ea ea ea

--- src/informationretrieval/Main.java
+++ src/informationretrieval/Main.java
@@ -16,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("sdcvbcbvbc");
+        System.out.println("sdcc");
         // TODO code application logic here
     }
     
